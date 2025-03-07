9fe4d3847d52c3cc5a82cee28a004aa32ffc854b
dcaayushd <aayushdc123@yahoo.com>
2025-03-07 14:00:31 +0545

    Var Keyword

--- Main/main.cs
+++ Main/main.cs
@@ -3,40 +3,11 @@ class MainProgram
 {
     public static void Main(string[] ___)
     {
-        // int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        // Random rand = new();
-        // numbers[0] = 11;
-        // numbers[5] = numbers[1];
-        // for (int i = 0; i < numbers.Length; i++)
-        // {
-        //     numbers[i] = rand.Next();
-        //     Console.Write(numbers[i] + " ");
-        // }
-        // Console.WriteLine();
-
-        object[] things  = {
-            new(),
-            new(),
-            new(),
-            new(),
-        };
-        Random rand = new();
-
-        things[0] = rand.Next();
-        things[1] = "second";
-        things[2] = 1.3456;
-        things[3] = 12;
-         for (int i = 0; i < things.Length; i++)
-        {   ;
-            Console.WriteLine(things[i]);
-        }
-        Console.WriteLine();
-
-        /*
-        To declare an array fo any size, we can use the following:
-        This will make an array of size 100 fro both types.
-        */
-        int[] numbers = new int[100];
-        object[] thing = new object[100];
+        var number = 12;
+        var str = "string";
+        var fltnum = 12.413534567f;
+        Console.WriteLine(number);
+        Console.WriteLine(str);
+        Console.WriteLine(fltnum);
     }
 }
--- Main/obj/Debug/net9.0/Main.AssemblyInfo.cs
+++ Main/obj/Debug/net9.0/Main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+375d5a027ebbc25d11ee1ab622bd46ca4126c5c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b24328329533a45a000877bc6dffe29bfdc4900")]
 [assembly: System.Reflection.AssemblyProductAttribute("Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
