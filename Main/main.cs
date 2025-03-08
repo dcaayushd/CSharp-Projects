755e8feae294957bf95d4ef9fad02d34ec598953
dcaayushd <aayushdc123@yahoo.com>
2025-03-08 19:18:46 +0545

    Return Values

--- Main/main.cs
+++ Main/main.cs
@@ -3,12 +3,31 @@ class MainProgram
 {
     public static void Main(string[] ___)
     {
-        // Hello("Aayush");
-        string n = "Aayush";
-        Hello(n);
+        int add = Sum(11, 22);
+        Console.WriteLine($"The sum of given two numbers is: {add}");
+        int div = Div(12, 3);
+        Console.WriteLine($"The quotient of given two numbers is: {div}");
     }
 
-    static void Hello(string name){
-        Console.WriteLine($"Hello {name},from the other side");
+    // static void Foo(){
+    //     if(false){
+    //         return ;
+    //     }
+            //Foo Code
+    // }
+    static int Sum(int a, int b)
+    {
+        // int result = a+b;
+        // return result;
+
+        return a + b;
+    }
+    static int Div(int a, int b)
+    {
+        // int result = a+b;
+        // return result;
+        if (b == 0) return 0;
+
+        return a / b;
     }
 }
